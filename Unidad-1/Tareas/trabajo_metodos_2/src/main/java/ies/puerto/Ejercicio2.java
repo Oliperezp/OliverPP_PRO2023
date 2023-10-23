4415cc455f873d3510046a70d29aa6e28f90f59e
Oliperezp <riaumf@gmail.com>
2023-10-23 16:20:46 +0100

    cambios en trabajo_metodos_2 Ejercicio2

--- Unidad-1/Tareas/trabajo_metodos_2/src/main/java/ies/puerto/Ejercicio2.java
+++ Unidad-1/Tareas/trabajo_metodos_2/src/main/java/ies/puerto/Ejercicio2.java
@@ -4,4 +4,16 @@ package ies.puerto;
 // then press Enter. You can now see whitespace characters in your code.
 public class Ejercicio2 {
 
+    //Realiza un programa que tenga un método al que se le pase un número e indique si es positivo o negativo y si es par o impar.
+
+    public boolean esPositivo(int numero){
+
+        boolean resultado=false;
+
+
+
+        return resultado;
+    }
+
+
 }
