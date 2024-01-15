94a40f8a7b5fba2a52d3abfcc6934ab293cfffa7
Oliperezp <riaumf@gmail.com>
2024-01-15 16:12:13 +0000

    ejercicios ejemplo Herencia

---
+++ Unidad-3/Herencia/src/main/java/Cuadrado.java
@@ -0,0 +1,15 @@
+public class Cuadrado extends Rectangulo{
+
+
+    public Cuadrado(){}
+
+    public Cuadrado(float base){
+        super(base,base);
+    }
+
+
+    @Override
+    public String toString(){
+        return "El lado del cuadrado es:" + getBase()+" ,y la altura: " + getAltura();
+    }
+}
---
+++ Unidad-3/Herencia/src/main/java/Rectangulo.java
@@ -0,0 +1,27 @@
+public class Rectangulo extends Figura{
+
+    public Rectangulo(){
+
+    }
+
+    public Rectangulo(float base, float altura) {
+        super(base, altura);
+    }
+
+    @Override
+    @Deprecated(since = "4,5",forRemoval = true)
+    public float calcularArea(){
+        return area();
+    }
+
+    public float area(){
+        return getBase()*getAltura();
+    }
+
+
+    @Override
+    public String toString(){
+        return "La Base del rectangulo es: "+getBase()+",y la Altura: "+getAltura();
+    }
+
+}
