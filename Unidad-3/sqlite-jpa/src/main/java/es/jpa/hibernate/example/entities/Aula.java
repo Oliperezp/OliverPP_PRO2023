f8c6911d36c4f9b1362bd8e81d720bc8e4e40b48
OliPerezp <oliverperezp87@gmail.com>
2024-04-18 16:41:05 +0100

    ejemplo de JPA

--- Unidad-3/sqlite-jpa/src/main/java/es/jpa/hibernate/example/entities/Aula.java
+++ Unidad-3/sqlite-jpa/src/main/java/es/jpa/hibernate/example/entities/Aula.java
@@ -15,12 +15,11 @@ public class Aula implements Serializable {
     @Column(name="id",nullable = false)
     private Integer id;
 
+    public Integer getId() {
+        return id;
+    }
 
-
-
-
-
-
-
-
+    public void setId(Integer id) {
+        this.id = id;
+    }
 }
